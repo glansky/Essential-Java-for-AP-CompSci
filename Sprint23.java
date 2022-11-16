dce96eee254155c1751314edfe54b874c82dd838
Gregory <67148056+glansky@users.noreply.github.com>
2022-11-16 14:30:07 +0700

    Methods
    This is within the method.

---
+++ Sprint23.java
@@ -0,0 +1,9 @@
+public class Sprint23 {
+    public static void main(String[] args){
+        displayMsg();
+    }
+
+    public static void displayMsg(){
+        System.out.println("This is within the method");
+    }
+}
