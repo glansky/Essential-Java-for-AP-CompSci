e701a20b93508832a7aae90526abbadfd34df413
Gregory <67148056+glansky@users.noreply.github.com>
2022-11-25 16:55:56 +0700

    The while Loop
    val: 0
    val: 1
    val: 2
    val: 3
    val: 4
    val: 5
    val: 6
    val: 7
    val: 8
    val: 9

---
+++ Sprint37.java
@@ -0,0 +1,14 @@
+/**
+ * Sprint 37 - The while Loop
+ */
+public class Sprint37 {
+    public static void main(String[] args){
+        int val = 0;
+
+        // This loop will run as long as the condition at the top is always true
+        while (val < 10){
+            System.out.println(String.format("val: %d",val));
+            val++;
+        }
+    }
+}
